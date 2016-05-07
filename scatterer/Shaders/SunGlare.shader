0b11edf478c82742f1c14e54541f97c87c317930
blackrack <feeldapowa@hotmail.com>
2016-05-07 16:24:56 +0200

    added additional ghost and doubled ghost instances for sunflare shader

--- scatterer/Shaders/SunGlare.shader
+++ scatterer/Shaders/SunGlare.shader
@@ -30,6 +30,7 @@
 			uniform sampler2D sunFlare;
 			uniform sampler2D sunGhost1;
 			uniform sampler2D sunGhost2;
+			uniform sampler2D sunGhost3;
 			
 			uniform sampler2D _customDepthTexture;  //depth buffer, to check if sun is behin terrain
 													//I would love it if this wasn't necessary, unfortunately, mountains far away
@@ -39,9 +40,16 @@
 			uniform float3 flareSettings;  //intensity, aspect ratio, scale
 			uniform float3 spikesSettings;
 			
-			uniform float4x4 ghost1Settings;// each row is an instance of a ghost
-			uniform float4x4 ghost2Settings;// for each row: intensity, aspect ratio, scale, position on sun-screenCenter line
-											// intensity of 0 means nothing defined
+			uniform float4x4 ghost1Settings1;// each row is an instance of a ghost
+			uniform float4x4 ghost1Settings2;// for each row: intensity, aspect ratio, scale, position on sun-screenCenter line
+											 // intensity of 0 means nothing defined
+			
+			uniform float4x4 ghost2Settings1;
+			uniform float4x4 ghost2Settings2;
+			
+			uniform float4x4 ghost3Settings1;
+			uniform float4x4 ghost3Settings2;
+											
 													
 													
 			uniform float Rg;
@@ -144,21 +152,34 @@
 				float2 toScreenCenter=sunViewPortPos.xy-0.5;
 			   	float3 ghosts=0;
 			
+				//ghost 1
 				for (int i=0; i<4; ++i)
     			{
-//        					if (ghost1Settings[i].x == 0)	break;
+        					ghosts+=ghost1Settings1[i].x * (tex2D(sunGhost1,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost1Settings1[i].w))*
+        					float2(aspectRatio*ghost1Settings1[i].y,1)*ghost1Settings1[i].z+0.5).rgb);
         					
-        					ghosts+=ghost1Settings[i].x * (tex2D(sunGhost1,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost1Settings[i].w))*
-        					float2(aspectRatio*ghost1Settings[i].y,1)*ghost1Settings[i].z+0.5).rgb);
+        					ghosts+=ghost1Settings2[i].x * (tex2D(sunGhost1,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost1Settings2[i].w))*
+        					float2(aspectRatio*ghost1Settings2[i].y,1)*ghost1Settings2[i].z+0.5).rgb);
+				}			
+	
+				//ghost 2
+				for (int j=0; j<4; ++j)
+    			{        					        					
+        					ghosts+=ghost2Settings1[j].x * (tex2D(sunGhost2,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost2Settings1[j].w))*
+        					float2(aspectRatio*ghost2Settings1[j].y,1)*ghost2Settings1[j].z+0.5).rgb);
+        					
+        					ghosts+=ghost2Settings2[j].x * (tex2D(sunGhost2,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost2Settings2[j].w))*
+        					float2(aspectRatio*ghost2Settings2[j].y,1)*ghost2Settings2[j].z+0.5).rgb);
 				}
 				
-				
-				for (int j=0; j<4; ++j)
-    			{
-//        					if (ghost2Settings[i].x == 0)	break;
-        					        					
-        					ghosts+=ghost2Settings[j].x * (tex2D(sunGhost2,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost2Settings[j].w))*
-        					float2(aspectRatio*ghost2Settings[j].y,1)*ghost2Settings[j].z+0.5).rgb);
+				//ghost 3
+				for (int k=0; k<4; ++k)
+    			{        					        					
+        					ghosts+=ghost3Settings1[k].x * (tex2D(sunGhost3,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost3Settings1[k].w))*
+        					float2(aspectRatio*ghost3Settings1[k].y,1)*ghost3Settings1[k].z+0.5).rgb);
+        					
+        					ghosts+=ghost3Settings2[k].x * (tex2D(sunGhost3,(IN.uv.xy-sunViewPortPos.xy+(toScreenCenter*ghost3Settings2[k].w))*
+        					float2(aspectRatio*ghost3Settings2[k].y,1)*ghost3Settings2[k].z+0.5).rgb);
 				}
 
 			   	ghosts=ghosts * smoothstep(0,1,1-length(toScreenCenter));
--- scatterer/SunFlare.cs
+++ scatterer/SunFlare.cs
@@ -20,8 +20,10 @@ namespace scatterer
 		
 		Texture2D sunSpikes;
 		Texture2D sunFlare;
+
 		Texture2D sunGhost1;
 		Texture2D sunGhost2;
+		Texture2D sunGhost3;
 
 		Manager lastActivePQS=null; //manager with last active PQS, for atmosphere transmittance data
 
@@ -46,25 +48,31 @@ namespace scatterer
 		Vector3 spikesSettings = Vector3.zero;
 	
 		[Persistent]
-		List<Vector4> ghost1SettingsList=new List<Vector4>{};
+		List<Vector4> ghost1SettingsList1=new List<Vector4>{};
 		[Persistent]
-		List<Vector4> ghost2SettingsList=new List<Vector4>{};
+		List<Vector4> ghost1SettingsList2=new List<Vector4>{};
+
+		[Persistent]
+		List<Vector4> ghost2SettingsList1=new List<Vector4>{};
+		[Persistent]
+		List<Vector4> ghost2SettingsList2=new List<Vector4>{};
+
+		[Persistent]
+		List<Vector4> ghost3SettingsList1=new List<Vector4>{};
+		[Persistent]
+		List<Vector4> ghost3SettingsList2=new List<Vector4>{};
 
 
 		public void start()
 		{
 			sunglareMaterial = new Material (ShaderTool.GetMatFromShader2 ("CompiledSunGlare.shader"));
 
-			sunSpikes = new Texture2D (2048, 2048);
-			sunFlare = new Texture2D (1024, 1024);
-			sunGhost1 = new Texture2D (128, 128);
-			sunGhost2 = new Texture2D (128, 128);
-
 			//Size is loaded automatically from the files
 			sunSpikes = new Texture2D (1, 1);
 			sunFlare = new Texture2D (1, 1);
 			sunGhost1 = new Texture2D (1, 1);
 			sunGhost2 = new Texture2D (1, 1);
+			sunGhost3 = new Texture2D (1, 1);
 			
 			
 			sunSpikes.LoadImage (System.IO.File.ReadAllBytes (String.Format ("{0}/{1}", inCore.path+"/sunflare", "sunSpikes.png")));
@@ -75,15 +83,19 @@ namespace scatterer
 			sunGhost1.wrapMode = TextureWrapMode.Clamp;
 			sunGhost2.LoadImage (System.IO.File.ReadAllBytes (String.Format ("{0}/{1}", inCore.path+"/sunflare", "Ghost2.png")));
 			sunGhost2.wrapMode = TextureWrapMode.Clamp;
+			sunGhost3.LoadImage (System.IO.File.ReadAllBytes (String.Format ("{0}/{1}", inCore.path+"/sunflare", "Ghost3.png")));
+			sunGhost3.wrapMode = TextureWrapMode.Clamp;
 
 //			sunglareMaterial.SetTexture ("_Sun_Glare", sunGlare);
 			sunglareMaterial.SetTexture ("sunSpikes", sunSpikes);
 			sunglareMaterial.SetTexture ("sunFlare", sunFlare);
 			sunglareMaterial.SetTexture ("sunGhost1", sunGhost1);
 			sunglareMaterial.SetTexture ("sunGhost2", sunGhost2);
+			sunglareMaterial.SetTexture ("sunGhost3", sunGhost2);
+
 			sunglareMaterial.SetTexture ("_customDepthTexture", inCore.customDepthBufferTexture);
 
-			sunglareMaterial.renderQueue = 3000;
+			sunglareMaterial.renderQueue = 3100;
 
 			screenMesh = MeshFactory.MakePlane (2, 2, MeshFactory.PLANE.XY, false, false);
 			screenMesh.bounds = new Bounds (Vector4.zero, new Vector3 (Mathf.Infinity, Mathf.Infinity, Mathf.Infinity));
@@ -94,23 +106,55 @@ namespace scatterer
 			loadConfigNode ();
 
 			//didn't want to serialize the matrices directly as the result is pretty unreadable
-			Matrix4x4 ghost1Settings = Matrix4x4.zero;
-			for (int i=0; i<ghost1SettingsList.Count; i++)
+			//sorry about the mess, I'll make a cleaner way later
+			//ghost 1
+			Matrix4x4 ghost1Settings1 = Matrix4x4.zero;
+			for (int i=0; i<ghost1SettingsList1.Count; i++)
 			{
-				ghost1Settings.SetRow(i,ghost1SettingsList[i]);
+				ghost1Settings1.SetRow(i,ghost1SettingsList1[i]);
+			}
+			Matrix4x4 ghost1Settings2 = Matrix4x4.zero;
+			for (int i=0; i<ghost1SettingsList2.Count; i++)
+			{
+				ghost1Settings2.SetRow(i,ghost1SettingsList2[i]);
 			}
 
-			Matrix4x4 ghost2Settings = Matrix4x4.zero;
-			for (int i=0; i<ghost2SettingsList.Count; i++)
+			//ghost 2
+			Matrix4x4 ghost2Settings1 = Matrix4x4.zero;
+			for (int i=0; i<ghost2SettingsList1.Count; i++)
 			{
-				ghost2Settings.SetRow(i,ghost2SettingsList[i]);
+				ghost2Settings1.SetRow(i,ghost2SettingsList1[i]);
+			}
+			Matrix4x4 ghost2Settings2 = Matrix4x4.zero;
+			for (int i=0; i<ghost2SettingsList2.Count; i++)
+			{
+				ghost2Settings2.SetRow(i,ghost2SettingsList2[i]);
+			}
+
+			//ghost 3
+			Matrix4x4 ghost3Settings1 = Matrix4x4.zero;
+			for (int i=0; i<ghost3SettingsList1.Count; i++)
+			{
+				ghost3Settings1.SetRow(i,ghost3SettingsList1[i]);
+			}
+			Matrix4x4 ghost3Settings2 = Matrix4x4.zero;
+			for (int i=0; i<ghost3SettingsList2.Count; i++)
+			{
+				ghost3Settings2.SetRow(i,ghost3SettingsList2[i]);
 			}
 
 
 			sunglareMaterial.SetVector ("flareSettings", flareSettings);
 			sunglareMaterial.SetVector ("spikesSettings", spikesSettings);
-			sunglareMaterial.SetMatrix ("ghost1Settings", ghost1Settings);
-			sunglareMaterial.SetMatrix ("ghost2Settings", ghost2Settings);
+
+			sunglareMaterial.SetMatrix ("ghost1Settings1", ghost1Settings1);
+			sunglareMaterial.SetMatrix ("ghost1Settings2", ghost1Settings2);
+
+			sunglareMaterial.SetMatrix ("ghost2Settings1", ghost2Settings1);
+			sunglareMaterial.SetMatrix ("ghost2Settings2", ghost2Settings2);
+
+			sunglareMaterial.SetMatrix ("ghost3Settings1", ghost3Settings1);
+			sunglareMaterial.SetMatrix ("ghost3Settings2", ghost3Settings2);
 
 			Debug.Log ("[Scatterer] added custom sun flare");
 
