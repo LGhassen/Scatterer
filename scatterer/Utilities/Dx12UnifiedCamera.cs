d6fd37d7a796635b4fc40015e0cba1b68f31c1a1
lghassen <feeldapowa@hotmail.com>
2022-02-26 00:16:36 +0100

    Force unified camera in Dx12, not very useful: Dx12 still runs slower than Dx11 (on both CPU and GPU), ocean shader also causes crash.

---
+++ scatterer/Utilities/Dx12UnifiedCamera.cs
@@ -0,0 +1,22 @@
+//Enables unified camera mode on Directx12, Dx12 still runs slower than Dx11 on both CPU and GPU for some reason (higher loads and lower framerates), also ocean shader causes crash
+using System;
+using UnityEngine;
+using System.Reflection;
+
+namespace scatterer
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, false)]
+	public class Dx12UnifiedCamera : MonoBehaviour
+	{
+		public void Start()
+		{
+			if (SystemInfo.graphicsDeviceVersion.Contains ("Direct3D 12"))
+			{
+				Type t = typeof(GameSettings);
+				BindingFlags Flags =  BindingFlags.FlattenHierarchy |  BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static;
+				t.GetField ("graphicsVersion", Flags).SetValue (null, GameSettings.GraphicsType.D3D11);
+			}
+		}
+	}
+}
+
