18ab68c95e54837aa8298ec872826e7edaf989f9
lghassen <11978271+LGhassen@users.noreply.github.com>
2026-08-15 22:10:39 +0200

    Bump version

--- scatterer/Scatterer.cs
+++ scatterer/Scatterer.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-[assembly:AssemblyVersion("0.0904")]
+[assembly:AssemblyVersion("0.0905")]
 namespace Scatterer
 {
     [KSPAddon(KSPAddon.Startup.EveryScene, false)]
@@ -47,7 +47,7 @@ namespace Scatterer
         bool coreInitiated = false;
         public bool isActive = false;
         public bool unifiedCameraMode = false;
-        public string versionNumber = "0.0904 dev";
+        public string versionNumber = "0.0905";
 
         public List<GenericAntiAliasing> antiAliasingScripts = new List<GenericAntiAliasing>();
 
